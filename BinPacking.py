467ae0cea31bf2c152f481b6653badc857320e9b
CarolHallaso <82183242+CarolHallaso@users.noreply.github.com>
2022-03-18 01:10:47 +0200

    Bin Packing - first fit + genetic done

--- BinPacking.py
+++ BinPacking.py
@@ -1,11 +1,10 @@
 import random
 import sys
-import math
-
+import time
 import numpy as np
 
 POPSIZE = 2048  # genome population size
-MAXITER = 16384  # maximum iterations (generations)
+MAXITER = 100  # maximum iterations
 ELITRATE = 0.1  # elitism rate
 MUTATIONRATE = 0.25  # mutation rate
 MUTATION = sys.maxsize * MUTATIONRATE
@@ -13,146 +12,155 @@ MUTATION = sys.maxsize * MUTATIONRATE
 class GA_struct:
 
     # Citizens of our Population
-    def __init__(self, string, fitness):
-        self.string = string
+    def __init__(self, permutation, fitness):
+        self.permutation = permutation
         self.fitness = fitness
 
 class BinPacking:
 
     def __init__(self, objects, Bsize):
-        self.NumOfObjects = len(objects)
         self.Bsize = Bsize
         self.objects = objects
+        self.NumOfObjects = len(objects)
 
-    def init_population(self, population: list, buffer: list):
+    def init_population(self, population: list):
 
         for i in range(POPSIZE):
-            citizen = GA_struct("", 0)
-            citizen.string = np.random.permutation(self.NumOfObjects)
+            citizen = GA_struct([], 0)
+            citizen.permutation = np.random.permutation(self.NumOfObjects)
             population[i] = citizen
         return
 
-    def calc_fitness(self, population):
-        c = self.Bsize
+    def calc_fitness(self, population: list):
+        capacity = self.Bsize
         n = self.NumOfObjects
         k = 2
-        N = len(set(self.objects))
+        for i in range(POPSIZE):
+            num_of_bins = len(set(population[i].permutation))
+            bins = [0] * n
+            sum = 0
 
-        for i in range(self.NumOfObjects):
-            fitness = pow((self.objects[i] / c), k)
-            fitness = fitness / N
+            for j in range(n):
+                sum += self.objects[j]
+                helper = population[i].permutation
+                bins[helper[j]] = sum
+
+            fitness = 0
+            for j in range(n):
+                if bins[j] != 0:
+                    fitness += pow(abs(capacity - bins[j]), k)
+
+            fitness /= num_of_bins
             population[i].fitness = fitness
         return
 
-    def sort_by_fitness(self, population):
+    def sort_by_fitness(self, population: list):
         population.sort(key=lambda x: x.fitness)
         return
 
-    def print_best(self, population):
-        print("Best: " + population[0].string + " fitness: " + " (" + str(population[0].fitness) + ")")
+    def print_best(self, population: list[GA_struct]):
+        print(f"Best till now: {len(set(population[0].permutation))} bins ")
         return
 
-    def elitism(self, population: list[GA_struct], buffer: list[GA_struct], esize):
-        temp = population[:esize].copy()
-        buffer[:esize] = temp
+    def elitism(self, population: list[GA_struct], buffer: list[GA_struct], size):
+        tmp = population[:size].copy()
+        buffer[:size] = tmp
         return
 
-    def mutate(self, member: GA_struct):
+    def mutate(self, citizen):
+        size = len(citizen.permutation)
+        index = random.randrange(0, size)
+        b = random.randrange(0, self.NumOfObjects)
+        citizen.permutation[index] = b
+        return citizen.permutation
 
-        t_size = len(member.string)
-        ipos = random.randrange(0, t_size - 1)
-        delta = random.randrange(0, 90) + 32
-        string = member.string[: ipos] + chr((ord(member.string[ipos]) + delta) % 122) + member.string[ipos + 1:]
-        member.string = string
-        return
+    def mate(self, population: list[GA_struct], buffer: list[GA_struct]):
+        size = int(POPSIZE * ELITRATE)
+        self.elitism(population, buffer, size)
+        for i in range(size, POPSIZE):
+            indx1 = random.randrange(1, self.NumOfObjects)
+            indx2 = random.randrange(indx1, self.NumOfObjects)
+            c1 = random.randrange(0, POPSIZE // 2)
+            c2 = random.randrange(0, POPSIZE // 2)
 
-    def mate(self, population: list[GA_struct], buffer: list[GA_struct], type):
-
-        esize = int(POPSIZE * ELITRATE)
-        self.elitism(population, buffer, esize)
-
-        # mate the rest
-        for i in range(esize, POPSIZE):
-
-            i1 = random.randrange(0, POPSIZE // 2)
-            i2 = random.randrange(0, POPSIZE // 2)
-
-            if type == "SINGLE":
-                pos = random.randrange(0, esize)
-                buffer[i] = GA_struct(population[i1].string[0: pos] + population[i2].string[pos:], 0)
-
-            elif type == "DOUBLE":
-                pos1 = random.randrange(0, esize - 2)
-                pos2 = random.randrange(pos1 + 1, esize - 1)
-                buffer[i] = GA_struct(
-                    population[i1].string[0: pos1] + population[i2].string[pos1:pos2] + population[i1].string[pos2:], 0)
-
-            elif type == "UNIFORM":
-                gen = ""
-                for j in range(esize):
-                    r = random.randrange(0, 2)
-                    if r == 0:
-                        gen = gen + population[i1].string[j]
-
-                    else:
-                        gen = gen + population[i2].string[j]
-                buffer[i] = GA_struct(gen, 0)
+            buffer[i] = GA_struct(
+                list(population[c1].permutation[0: indx1]) + list(population[c2].permutation[indx1:indx2]) + list(population[c1].permutation[indx2:]), 0)
 
             if random.randrange(sys.maxsize) < MUTATION:
                 self.mutate(buffer[i])
-
         return
 
     def swap(self, population: list[GA_struct], buffer: list[GA_struct]):
 
         return buffer, population
 
+    def genetic(self, objects, capacity):
+        time0 = time.time()
+        random.seed()
+        pop1 = [None] * POPSIZE
+        pop2 = [None] * POPSIZE
+        self.init_population(pop1)
+        population = pop1
+        buffer = pop2
+        for i in range(MAXITER):
+            self.calc_fitness(population)
+            self.sort_by_fitness(population)
+            print("Genetic algorithm -")
+            self.print_best(population)
+            self.mate(population, buffer)
+            population, buffer = self.swap(population, buffer)
+
+        elapsed_time = time.time() - time0
+        print("Genetic Algorithm Elapsed time = " + str(elapsed_time))
+        return len(set(population[0].permutation))
+
 
 # Returns number of bins required using first fit algorithm
-def firstFit(weights, Bins_capacity):
-    # Initialize result (Count of bins)
-    result = 0
-    num_of_objects = len(weights)
-    # Create an array to store remaining space in bins
-    # there can be at most n bins
-    bin_rem = [0] * num_of_objects
+def firstFit(objects, capacity):
+        time0 = time.time()  # Create an array to store remaining space in bins
+        # there can be at most n bins
+        bin_rem = [0] * num_of_objects
 
-    for i in range(num_of_objects):
-        # Find the first bin that has enough space
-        j = 0
-        while j < result:
-            if bin_rem[j] >= weights[i]:
-                bin_rem[j] = bin_rem[j] - weights[i]
-                break
-            j += 1
+        # Initialize result (Count of bins)
+        result = 0
+        for i in range(num_of_objects):
+            # Find the first bin that has enough space
+            j = 0
+            while j < result:
+                if bin_rem[j] >= objects[i]:
+                    bin_rem[j] = bin_rem[j] - objects[i]
+                    break
+                j += 1
 
-        # If no bin could accommodate weight[i]
-        if j == result:
-            bin_rem[result] = Bins_capacity - weights[i]
-            result = result + 1
-    return result
+            # If no bin could accommodate weight[i]
+            if j == result:
+                bin_rem[result] = capacity - objects[i]
+                result = result + 1
 
+        elapse_time = time.time()-time0
+        print("First Fit Algorithm - Elapsed Time = " + str(elapse_time))
+        return result
 
 if __name__ == "__main__":
 
-    weights = [2, 5, 4, 7, 1, 3, 8]
-    c = 10
-
-    print("Number of bins required in First Fit : ", firstFit(weights,c))
-
-    problem = BinPacking(weights, c)
     random.seed()
-    pop_alpha = [None] * POPSIZE
-    pop_beta = [None] * POPSIZE
-    problem.init_population(pop_alpha, pop_beta)
-    population = pop_alpha
-    buffer = pop_beta
+    num_of_objects = 50
+    capacity = 100
+    objects = [0] * num_of_objects
+    for i in range(num_of_objects):
+        # each object weigh can be <= capacity
+        objects[i] = random.randrange(1, capacity + 1)
 
-    for i in range(MAXITER):
-        problem.calc_fitness(population)
-        problem.sort_by_fitness(population)
-        print("num of required bins according to genetic algorithm")
-        problem.print_best(population)
+    print("Number of bins required in - First fit Algorithm:")
+    result1 = firstFit(objects, capacity)
+    print(result1)
 
-        problem.mate(population, buffer, "UNIFORM")
-        population, buffer = problem.swap(population, buffer)
+    problem = BinPacking(objects, capacity)
+    result2 = problem.genetic(objects, capacity)
+    print("Number of bins required in - Genetic Algorithm:")
+    print(result2)
+
+
+
+
+
